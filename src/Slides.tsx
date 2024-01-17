1863d808d9346004cb0f025eeb75fe780824d73f
Ricardo Romo <jricardoromo21@gmail.com>
2024-01-17 10:44:29 -0500

    add tailwind, fix buton styles

--- src/Slides.tsx
+++ src/Slides.tsx
@@ -1,47 +1,67 @@
-import { useRef, useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import './Slide.css'
+import { Maximize, Minimize } from "lucide-react";
 function Slide() {
 
     const [isFullscreen, setIsFullscreen] = useState(false)
+    const btnFs = "text-black-500 fixed bottom-[2vh] right-[2vw] hover:bg-white w-[40px] h-[40px] p-2 rounded-lg ease-in-out transition-all duration-300"
+    const elementRef: any = useRef(null)
 
-    const elementRef = useRef(null)
-
-    const handleFullscreen = async () =>{
-        if(!elementRef) return
+    const handleFullscreen = async () => {
+        if (!elementRef) return
         const res = await elementRef?.current?.requestFullscreen()
         setIsFullscreen(true)
         console.log(res);
     }
-    const handleFullscreenOut = async () =>{
-        if(!elementRef) return
+
+
+    const handleFullscreenOut = async () => {
+        if (!elementRef) return
         document.exitFullscreen();
         setIsFullscreen(false)
     }
 
+    useEffect(() => {
+        const handle = () => {
+            if (document.fullscreenElement) {
+                setIsFullscreen(true)
+            } else {
+                setIsFullscreen(false)
+            }
+        }
+        document.addEventListener("fullscreenchange", handle);
+        return () => {
+            document.removeEventListener("fullscreenchange", handle);
+        };
+    }, []);
 
     return (
-    <div id="slide">
-        {/* <FullScreen handle={handle}> */}
+        <div id="slide">
+            {/* <FullScreen handle={handle}> */}
             <div
                 ref={elementRef}
-                >
-                <img src="https://picsum.photos/536/354" alt=""/> 
+            >
+                <img src="https://picsum.photos/536/354" alt="" />
 
-            {/* <iframe width="100%" height="100%" src="https://www.youtube.com/embed/-SJfoBFjdnQ" title="Fania All Stars Live in Africa - Full Movie" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-        {isFullscreen && <button onClick={handleFullscreenOut}><img src="/minimize.svg"></img></button>}
+                {/* <iframe width="100%" height="100%" src="https://www.youtube.com/embed/-SJfoBFjdnQ" title="Fania All Stars Live in Africa - Full Movie" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+                {isFullscreen &&
+                    <button
+                        className={btnFs}
+                        onClick={handleFullscreenOut}
+                    >
+                        <Minimize className="hover:stroke-black" />
+                    </button>}
             </div>
-        
-        {/* </FullScreen> */}
-        {!isFullscreen && <button onClick={handleFullscreen}><img src="/maximize.svg"></img></button>}
-        
-    </div>
-    // <div id="slide">
-    //     <FullScreen handle={handle}>
-    //        <img src="https://picsum.photos/536/354" alt=""/>
-    //     </FullScreen>
-        
 
-    // </div>
+            {/* </FullScreen> */}
+            {!isFullscreen &&
+                <button
+                    className={btnFs}
+                    onClick={handleFullscreen}
+                >
+                    <Maximize className="hover:stroke-black" />
+                </button>}
+        </div>
     )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,82 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {
+      animation: {
+        // Fade up and down
+        "fade-up": "fade-up 0.5s",
+        "fade-down": "fade-down 0.5s",
+        // Tooltip
+        "slide-up-fade": "slide-up-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+        "slide-down-fade": "slide-down-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideLeftAndFade: 'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideRightAndFade: 'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
+      keyframes: {
+        // Fade up and down
+        "fade-up": {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(10px)",
+          },
+          "80%": {
+            opacity: 0.6,
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0px)",
+          },
+        },
+        "fade-down": {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(-10px)",
+          },
+          "80%": {
+            opacity: 0.6,
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0px)",
+          },
+        },
+        // Tooltip
+        "slide-up-fade": {
+          "0%": { opacity: 0, transform: "translateY(6px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        "slide-down-fade": {
+          "0%": { opacity: 0, transform: "translateY(-6px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideLeftAndFade: {
+          from: { opacity: 0, transform: 'translateX(2px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        slideUpAndFade: {
+          from: { opacity: 0, transform: 'translateY(2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideRightAndFade: {
+          from: { opacity: 0, transform: 'translateX(-2px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('tailwindcss'),
+  ],
+};
