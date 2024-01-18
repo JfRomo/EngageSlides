e878584ffd7b2d129a4c3c1f8a4cfd1d2fe01ca9
JfRomo <jfromo98@gmail.com>
2024-01-17 20:42:28 -0500

    added states to the context

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ function App() {
     <>
       <div>
       <Slide/>  
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       </div>
     </>
   )
--- src/Providers/SlideProvider.tsx
+++ src/Providers/SlideProvider.tsx
@@ -2,21 +2,50 @@ import { ReactNode, createContext, useState } from "react";
 
 interface SlideContextPros {
     isFullscreen: boolean,
-    setIsFullscreen: (value: boolean) => void
+    setIsFullscreen: (value: boolean) => void,
+    viewNotifications: boolean,
+    setViewNotifications: (value: boolean) => void,
+    viewSidebar: boolean,
+    setViewSidebar: (value: boolean) => void,
+    shadowRange: boolean,
+    setShadowRange: (value: boolean) => void,
+    messagesArray: any[],
+    setMessagesArray: (value: any[]) => void,
+    slide: number,
+    setSlide: (value: number)
 }
 
 
 export const SlideContext = createContext<SlideContextPros>({
     isFullscreen: false,
-    setIsFullscreen: () => { }
+    setIsFullscreen: () => { },
+
 })
 
 export const SlideProvider = ({ children }: { children: ReactNode }) => {
 
     const [isFullscreen, setIsFullscreen] = useState(false)
+    const [viewNotifications, setViewNotifications] = useState<boolean>(false);
+    const [viewSidebar, setViewSidebar] = useState<boolean>(false);
+    const [shadowRange, setShadowRange] = useState<boolean>(false);
+    const [messagesArray, setMessagesArray] = useState<any[]>([]); // Adjust the type according to your needs
+    const [slide, setSlide] = useState<number>(0);
 
     return <SlideContext.Provider
-        value={{ isFullscreen, setIsFullscreen }}
+        value={{
+            isFullscreen, 
+            setIsFullscreen, 
+            viewNotifications,
+            setViewNotifications,
+            viewSidebar,
+            setViewSidebar,
+            shadowRange,
+            setShadowRange,
+            messagesArray,
+            setMessagesArray,
+            slide,
+            setSlide,
+        }}
     >
         {children}
     </SlideContext.Provider>
