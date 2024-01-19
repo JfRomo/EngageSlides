4585cc2f818594c2a7b7a53ce18987c4bbf72fc7
Ricardo Romo <jricardoromo21@gmail.com>
2024-01-19 09:32:43 -0500

    add message interval

--- src/Providers/SlideProvider.tsx
+++ src/Providers/SlideProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
 
 interface SlideContextPros {
     isFullscreen: boolean,
@@ -7,19 +7,29 @@ interface SlideContextPros {
     setViewNotifications: (value: boolean) => void,
     viewSidebar: boolean,
     setViewSidebar: (value: boolean) => void,
-    shadowRange: boolean,
-    setShadowRange: (value: boolean) => void,
-    messagesArray: any[],
-    setMessagesArray: (value: any[]) => void,
     slide: number,
-    setSlide: (value: number)
+    setSlide: (value: number) => void,
+    shadowRange: number,
+    messagesArray: any[],
 }
 
+interface message {
+    name: string,
+    message: string,
+    dateTime: string,
+}
 
 export const SlideContext = createContext<SlideContextPros>({
     isFullscreen: false,
     setIsFullscreen: () => { },
-
+    viewNotifications: false,
+    setViewNotifications: () => { },
+    viewSidebar: false,
+    setViewSidebar: () => { },
+    slide: 0,
+    setSlide: () => { },
+    shadowRange: 4,
+    messagesArray: [],
 })
 
 export const SlideProvider = ({ children }: { children: ReactNode }) => {
@@ -27,22 +37,28 @@ export const SlideProvider = ({ children }: { children: ReactNode }) => {
     const [isFullscreen, setIsFullscreen] = useState(false)
     const [viewNotifications, setViewNotifications] = useState<boolean>(false);
     const [viewSidebar, setViewSidebar] = useState<boolean>(false);
-    const [shadowRange, setShadowRange] = useState<boolean>(false);
-    const [messagesArray, setMessagesArray] = useState<any[]>([]); // Adjust the type according to your needs
+    const [shadowRange, setShadowRange] = useState<number>(4);
+    const [messagesArray, setMessagesArray] = useState<message[]>([]); // Adjust the type according to your needs
     const [slide, setSlide] = useState<number>(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            
+        }, 1000)
+
+        return clearInterval(interval)
+    }, [])
 
     return <SlideContext.Provider
         value={{
-            isFullscreen, 
-            setIsFullscreen, 
+            isFullscreen,
+            setIsFullscreen,
             viewNotifications,
             setViewNotifications,
             viewSidebar,
             setViewSidebar,
             shadowRange,
-            setShadowRange,
             messagesArray,
-            setMessagesArray,
             slide,
             setSlide,
         }}
